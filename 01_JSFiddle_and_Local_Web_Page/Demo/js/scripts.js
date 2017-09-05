3ae73b35cf3d51c630eb65d05278d3e72e89cb12
Robert Frenette <robert.frenette@comcast.net>
2017-09-05 18:13:46 -0400

    Added content for Section 1.

---
+++ 01_JSFiddle_and_Local_Web_Page/Demo/js/scripts.js
@@ -0,0 +1,34 @@
+// This is a single line comment.
+
+/*
+  This is a multi line comment.
+  Orig Fiddle: https://jsfiddle.net/RMFrenette/3qksqf4g/
+*/
+
+// clear the JavaScript Console
+console.clear();
+
+// write greeting to the console
+console.log('Hello, World!');
+
+// declare varaible to hold greeting.
+var greeting = 'Hello, World!';
+
+// write greeting to console using data in var
+console.log(greeting);
+
+// alert greeting
+alert(greeting);
+
+// prompt user for first name
+var firstName = prompt('Please enter your First Name.');
+// greet user
+alert('Hello, ' + firstName + '!');
+
+// confirm dialog
+var userResponse = confirm("Press a button!");
+console.log('You clicked: ' + userResponse);
+
+// reuse existing var
+userResponse = confirm("Press the other button!");
+console.log('You clicked: ' + userResponse);
---
+++ 01_JSFiddle_and_Local_Web_Page/README.md
@@ -0,0 +1,8 @@
+## JSFiddle and Local Web Page
+
+This Section covers JSFiddle and "converting" a Fiddle to a local web page.
+
+### JSFiddle:
+
+ * [E-3_Fall_2017: Fiddle 1](https://jsfiddle.net/RMFrenette/3qksqf4g/) JavaScript Console and Dialog Boxes
+ 
